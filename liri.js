1c2cd2358f4f1cbdba3cd10a64e1fbdb95b4a0c7
pholmes10 <pholmes10@gmail.com>
2018-12-07 10:03:30 -0600

    added if/else statements to show default song, movie etc.

--- liri.js
+++ liri.js
@@ -13,33 +13,70 @@ var userOption = process.argv[3];
 
 switch (userChoice) {
     case "concert-this":
-        console.log("concert");
-        break;
-    case "spotify-this-song":
-        spotify
-            .search({ type: 'track', query: userOption, limit: 1 })
-            .then(function (response) {
-                console.log(response.tracks.items[0].artists[0].name);
-                console.log(response.tracks.items[0].name);
-                console.log(response.tracks.items[0].preview_url);
-                console.log(response.tracks.items[0].album.name);
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-        break;
-    case "movie-this":
-        axios.get("http://www.omdbapi.com/?t=" + userOption + "&y=&plot=short&apikey=trilogy").then(
+        axios.get("https://rest.bandsintown.com/artists/" + userOption + "/events?app_id=9f4af23467b3f212bd77b85d490a9d5a").then(
             function (response) {
-                console.log(response.data.Title);
-                console.log(response.data.Year);
-                console.log(response.data.Ratings.imdbRating);
-                console.log(response.data.Country);
-                console.log(response.data.Language);
-                console.log(response.data.Plot);
-                console.log(response.data.Actors);
+                console.log(response.data[0].venue.name);
+                console.log(`${response.data[0].venue.city}, ${response.data[0].venue.country}`);
+                console.log(moment(response.data[0].datetime).format('L'));
             }
         );
+        break;
+    case "spotify-this-song":
+        if (userOption === undefined) {
+            spotify
+                .search({ type: 'track', query: "The Sign", limit: 1 })
+                .then(function (response) {
+                    console.log(response.tracks.items[0].artists[0].name);
+                    console.log(response.tracks.items[0].name);
+                    console.log(response.tracks.items[0].preview_url);
+                    console.log(response.tracks.items[0].album.name);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        } else {
+            spotify
+                .search({ type: 'track', query: userOption, limit: 1 })
+                .then(function (response) {
+                    console.log(response.tracks.items[0].artists[0].name);
+                    console.log(response.tracks.items[0].name);
+                    console.log(response.tracks.items[0].preview_url);
+                    console.log(response.tracks.items[0].album.name);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        };
+        break;
+    case "movie-this":
+        console.log(userOption)
+        if (userOption === undefined) {
+            axios.get("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy").then(
+                function (response) {
+                    console.log(response.data.Title);
+                    console.log(response.data.Year);
+                    console.log(response.data.imdbRating);
+                    console.log(response.data.Ratings[1].Value);
+                    console.log(response.data.Country);
+                    console.log(response.data.Language);
+                    console.log(response.data.Plot);
+                    console.log(response.data.Actors);
+                }
+            );
+        } else {
+            axios.get("http://www.omdbapi.com/?t=" + userOption + "&y=&plot=short&apikey=trilogy").then(
+                function (response) {
+                    console.log(response.data.Title);
+                    console.log(response.data.Year);
+                    console.log(response.data.imdbRating);
+                    console.log(response.data.Ratings[1].Value);
+                    console.log(response.data.Country);
+                    console.log(response.data.Language);
+                    console.log(response.data.Plot);
+                    console.log(response.data.Actors);
+                }
+            );
+        };
         break;
     case "do-what-it-says":
         fs.readFile("random.txt", "utf8", function (error, data) {
@@ -55,34 +92,69 @@ switch (userChoice) {
 
             switch (userChoice) {
                 case "concert-this":
-                    console.log("concert");
-                    break;
-                case "spotify-this-song":
-                    spotify
-                        .search({ type: 'track', query: userOption, limit: 1 })
-                        .then(function (response) {
-                            console.log(response.tracks.items[0].artists[0].name);
-                            console.log(response.tracks.items[0].name);
-                            console.log(response.tracks.items[0].preview_url);
-                            console.log(response.tracks.items[0].album.name);
-                        })
-                        .catch(function (err) {
-                            console.log(err);
-                        });
-                    break;
-                case "movie-this":
-                    axios.get("http://www.omdbapi.com/?t=" + userOption + "&y=&plot=short&apikey=trilogy").then(
+                    axios.get("https://rest.bandsintown.com/artists/" + userOption + "/events?app_id=9f4af23467b3f212bd77b85d490a9d5a").then(
                         function (response) {
-                            console.log(response.data.Title);
-                            console.log(response.data.Year);
-                            console.log(response.data.imdbRating);
-                            console.log(response.data.Ratings[1].Value);
-                            console.log(response.data.Country);
-                            console.log(response.data.Language);
-                            console.log(response.data.Plot);
-                            console.log(response.data.Actors);
+                            console.log(response.data[0].venue.name);
+                            console.log(`${response.data[0].venue.city}, ${response.data[0].venue.country}`);
+                            console.log(moment(response.data[0].datetime).format('L'));
                         }
                     );
+                    break;
+                case "spotify-this-song":
+                    if (userOption === undefined) {
+                        spotify
+                            .search({ type: 'track', query: "The Sign", limit: 1 })
+                            .then(function (response) {
+                                console.log(response.tracks.items[0].artists[0].name);
+                                console.log(response.tracks.items[0].name);
+                                console.log(response.tracks.items[0].preview_url);
+                                console.log(response.tracks.items[0].album.name);
+                            })
+                            .catch(function (err) {
+                                console.log(err);
+                            });
+                    } else {
+                        spotify
+                            .search({ type: 'track', query: userOption, limit: 1 })
+                            .then(function (response) {
+                                console.log(response.tracks.items[0].artists[0].name);
+                                console.log(response.tracks.items[0].name);
+                                console.log(response.tracks.items[0].preview_url);
+                                console.log(response.tracks.items[0].album.name);
+                            })
+                            .catch(function (err) {
+                                console.log(err);
+                            });
+                    };
+                    break;
+                case "movie-this":
+                    if (userOption === undefined) {
+                        axios.get("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy").then(
+                            function (response) {
+                                console.log(response.data.Title);
+                                console.log(response.data.Year);
+                                console.log(response.data.imdbRating);
+                                console.log(response.data.Ratings[1].Value);
+                                console.log(response.data.Country);
+                                console.log(response.data.Language);
+                                console.log(response.data.Plot);
+                                console.log(response.data.Actors);
+                            }
+                        );
+                    } else {
+                        axios.get("http://www.omdbapi.com/?t=" + userOption + "&y=&plot=short&apikey=trilogy").then(
+                            function (response) {
+                                console.log(response.data.Title);
+                                console.log(response.data.Year);
+                                console.log(response.data.imdbRating);
+                                console.log(response.data.Ratings[1].Value);
+                                console.log(response.data.Country);
+                                console.log(response.data.Language);
+                                console.log(response.data.Plot);
+                                console.log(response.data.Actors);
+                            }
+                        );
+                    };
                     break;
                 default:
                     console.log("please enter valid input");
@@ -93,4 +165,9 @@ switch (userChoice) {
     default:
         console.log("please enter valid input");
         break;
-}
+};
+
+
+
+
+
